6b31b7624126fe55df2a995173531eec5e33724e
Cesar F <beguface@gmail.com>
2021-03-01 13:11:49 -0400

    details book view ready

--- src/app/library/pages/book-details/book-details.component.ts
+++ src/app/library/pages/book-details/book-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../../interfaces/book.interface';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-book-details',
@@ -8,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  book!: Book;
+  id!: number;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private booksService: BooksService,
+    private _location: Location) { }
+
 
   ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe(({id }) => {
+
+      this.book = this.booksService.getDetailsBook( id )
+
+    })
+
   }
+
+
+  rentBook(id: number) {
+
+    this.id = id;
+    this.book = this.booksService.rentBook(this.id);
+  }
+
+  backClicked() {
+    this._location.back();
+  }
+
 
 }
